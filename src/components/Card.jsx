011bb746bcac1c910c1aaa5bf510c8328f945cb5
Govindyadav-dev <ygovind887430@gmail.com>
2026-05-20 19:36:36 +0530

    updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ import { toast } from 'react-toastify';
 function Card({name,image,id,price,type}) {
    let dispatch=useDispatch() 
   return (
-    <div className='w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg hover:border-2 border-green-300 '>
+    <div className='w-[400px] h-[500px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg hover:border-2 border-green-300 '>
       <div className='w-[100%] h-[60%] overflow-hidden rounded-lg'>
 <img src={image} alt="" className='object-cover'/>
       </div>
