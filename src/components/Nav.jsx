26ff5e29d78d9a6c28f3fd2e38994bbf42923f21
Govindyadav-dev <ygovind887430@gmail.com>
2026-05-22 18:21:27 +0530

    updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,17 +16,17 @@ function Nav() {
     
   return (
     <div className='w-full h-[100px] flex justify-between items-center px-5 md:px-8'>
-      <div className='w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl'>
+      <div className='w-[80px] h-[80px] bg-white flex justify-center items-center rounded-md shadow-xl'>
       <MdFastfood className='w-[30px] h-[30px] text-green-500'/>
       </div>
       <form className='w-[45%] h-[60px] bg-white flex items-center px-5 gap-5 rounded-md shadow-md md:w-[70%]' onSubmit={(e)=>e.preventDefault()}>
       <IoSearch  className='text-green-500 w-[20px] h-[20px] '/>
       <input type="text" placeholder='Search Items...'  className='w-[100%] outline-none text-[16px] md:text-[20px]'  onChange={(e)=>setInput(e.target.value)} value={input}/>
       </form>
-      <div className='w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl relative cursor-pointer' onClick={()=>{
+      <div className='w-[80px] h-[80px] bg-white flex justify-center items-center rounded-md shadow-xl relative cursor-pointer' onClick={()=>{
         setShowCart(true)
       }}>
-        <span className='absolute top-0 right-2 text-green-500 font-bold text-[18px]'>{items.length}</span>
+        <span className='absolute top-0 right-2 text-orange-500 font-bold text-[18px]'>{items.length}</span>
       <LuShoppingBag className='w-[30px] h-[30px] text-green-500 '/>
       </div>
     </div>
